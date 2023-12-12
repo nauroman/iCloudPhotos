9aa69bb9f9b2895dc156d54102311e1fb188e6b1
Roman Naumov <nauroman@gmail.com>
2023-12-12 09:58:43 -0800

    Add files via upload

---
+++ iCloudPhotos.ps1
@@ -0,0 +1,26 @@
+$folderPath = "C:\Users\user\Downloads"
+$zipName = "iCloud Photos.zip"
+$tempExtractPath = "C:\Users\user\Downloads\iCloud Photos" # Use a dedicated folder for extraction
+
+while ($true) {
+    $zipFile = Get-ChildItem -Path $folderPath -Filter $zipName -ErrorAction SilentlyContinue
+    if ($zipFile) {
+        Write-Host "Found zip file: $($zipFile.FullName)"
+
+        # Use a separate, safe location for extraction
+        Expand-Archive -LiteralPath $zipFile.FullName -DestinationPath $tempExtractPath -Force
+        
+        # Move only JPEG files back to the original folder
+        Get-ChildItem -Path $tempExtractPath -Include "*.jpeg", "*.jpg", "*.png", "*.gif" -Recurse | 
+            Copy-Item -Destination $folderPath -ErrorAction SilentlyContinue
+
+        # Remove the zip file after processing
+        Remove-Item -LiteralPath $zipFile.FullName -Force -ErrorAction SilentlyContinue
+
+        Remove-Item -Path $tempExtractPath -Recurse -Force -ErrorAction SilentlyContinue
+
+        Write-Host "Processed zip file and cleaned up temporary folder."
+    }
+
+    Start-Sleep -Seconds 3
+}
